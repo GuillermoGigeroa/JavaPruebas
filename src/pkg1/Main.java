3bdfa74381068a974c9c751e7d469c28196c743c
guilleGigeroa <guillermo.gigeroa@hotmail.com>
2021-08-09 10:44:13 -0300

    Pruebas

--- src/pkg1/Main.java
+++ src/pkg1/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		Planta planta1 = new Planta("Bebecita", "Amarillo");
 		Planta planta2 = new Planta("Planta2");
 		Planta planta3 = new Planta();
-
+		// Incluir regador que reciba la planta y la pueda regar. Y esto modifique el
+		// estado de la planta
 		View.MostrarPlanta(planta1);
 		View.MostrarPlanta(planta2);
 		View.MostrarPlanta(planta3);
