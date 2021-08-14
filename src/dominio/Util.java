8605d007ed034b2f77b0796983c6aaf1d1ff1b47
guilleGigeroa <guillermo.gigeroa@hotmail.com>
2021-08-14 16:28:25 -0300

    pushConPython

--- src/dominio/Util.java
+++ src/dominio/Util.java
@@ -1,28 +1,14 @@
 package dominio;
 
 public class Util {
-	public static void Escribir(String esto) {
-		System.out.println(esto);
-	}
-
-	public static void Linea() {
-		System.out.println();
-	}
-	
-	public static void Raya() {
-		String raya = "";
-		int cantidad = 50;
-		for (int x = 0; x < cantidad; x = x+1) {
-			raya += "-";
-		}
-		System.out.println(raya);
-	}
-	
-	public static void Raya(int cantidad) {
-		String raya = "";
-		for (int x = 0; x < cantidad; x = x+1) {
-			raya += "-";
-		}
-		System.out.println(raya);
+	public static void Escribir(String esto) {System.out.println(esto);}
+	public static void Linea() {System.out.println();}
+	public static void Raya(int cantidadCaracteres) {Escribir(Guiones(cantidadCaracteres));}
+	public static void Raya() {Escribir(Guiones());}
+	private static String Guiones() {return Guiones(50);}
+	private static String Guiones(int cantidad) {
+		String texto = "";
+		for (int x = 0; x < cantidad; x = x + 1) {texto += "-";}
+		return texto;
 	}
 }
